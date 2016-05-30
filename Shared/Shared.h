68b33bdc71a61d7041912f25f2dfd4f936b14be1
Thomas Visser <thomas.visser@gmail.com>
2016-05-30 14:56:42 +0200

    Fixed incompatibility with CocoaPods 1.0 / Swift

--- Shared/Shared.h
+++ Shared/Shared.h
@@ -6,16 +6,14 @@
 //  Copyright (c) 2015 Highstreet. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 //! Project version number for Shared.
 FOUNDATION_EXPORT double SharedVersionNumber;
 
 //! Project version string for Shared.
 FOUNDATION_EXPORT const unsigned char SharedVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Shared/PublicHeader.h>
 
+// In this header, you should import all the public headers of your framework
 
-#import <Shared/HSPushDefinitions.h>
-#import <Shared/HSWatchKitDefinitions.h>
+#import "HSPushDefinitions.h"
+#import "HSWatchKitDefinitions.h"
